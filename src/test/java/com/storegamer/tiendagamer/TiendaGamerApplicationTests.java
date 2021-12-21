d5bd0f4dd6f4ad0a3357256e2bf29fab0858914f
jhordan camacho <jhordan.camacho@gmail.com>
2021-12-21 13:34:15 -0500

    primer commit

---
+++ src/test/java/com/storegamer/tiendagamer/TiendaGamerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.storegamer.tiendagamer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaGamerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
